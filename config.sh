58b49992d5d3e1bcc1dd655fa97877ddd4a57ab2
l1f21bsse0036 <l1f21bsse0036@gmail.com>
2025-01-30 11:28:21 +0500

    initial commit

---
+++ config.sh
@@ -0,0 +1,6 @@
+echo "Going to Config the Repo"
+git branch -m develop
+git config --local user.name "l1f21bsse0036"
+git config --local user.email "l1f21bsse0036@gmail.com"
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
